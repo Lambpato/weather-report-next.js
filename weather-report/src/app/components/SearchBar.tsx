f45a4030691027a21c0a64e1721346378b2a5d76
Lamberto Martinez <lambpato@gmail.com>
2023-12-05 04:14:47 +0000

    updated getSearch and SearchBar

--- weather-report/src/app/components/SearchBar.tsx
+++ weather-report/src/app/components/SearchBar.tsx
@@ -1,22 +1,31 @@
 'use client';
 
-import { ChangeEvent, useState } from 'react';
-import { useWeather } from '../lib';
+import { ChangeEvent, useEffect, useState } from 'react';
+import { getSearch } from '../lib';
 
 export default function SearchBar() {
-  const [inputValue, setInputValue] = useState(String);
-  const [city, setCity] = useState(Object);
-  const { getSearch } = useWeather;
+  const [inputValue, setInputValue] = useState('');
+  const [city, setCity] = useState(undefined);
 
-  const onInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+  const onInputChange = async (e: ChangeEvent<HTMLInputElement>) => {
     setInputValue(e.target.value);
-    setCity(getSearch(inputValue));
+    try {
+      const result = await getSearch(e.target.value);
+      console.log(result);
+      setCity(result);
+    } catch (error) {
+      console.error('Error fetching search results:', error);
+    }
   };
 
   return (
     <div>
-      <input type='text' value={inputValue} onChange={onInputChange}></input>
-      <p></p>
+      <input
+        className='text-black'
+        type='text'
+        value={inputValue}
+        onChange={onInputChange}
+      />
     </div>
   );
 }
--- weather-report/src/app/lib/index.ts
+++ weather-report/src/app/lib/index.ts
@@ -1,22 +1,10 @@
-import { useState } from "react";
-
-type queryProps = {
-  input: string
-}
-
-export const useWeather = () => {
-  const [options, setOptions] = useState<[]>([]);
-
-  const getSearch = async (props: queryProps) => {
-    try {
-      const response = await fetch(`https://api.weatherapi.com/v1/search.json?q=${props.input}&key=bb2468a183ea4225855173630232702`);
-      if(response.ok!) throw new Error(`Something bad happened ${response.status}`);
-      const jsonData = await response.json();
-      setOptions(jsonData);
-    } catch (e) {
-      console.error(e);
-    }
+export const getSearch = async (query: string) => {
+  try {
+    const response = await fetch(`https://api.weatherapi.com/v1/search.json?q=${query}&key=bb2468a183ea4225855173630232702`);
+    if (!response.ok) throw new Error(`Something bad happened ${response.status}`);
+    const jsonData = await response.json();
+    return jsonData;
+  } catch (e) {
+    console.error(e);
   }
-
-  return {getSearch, options};
-}
+};
