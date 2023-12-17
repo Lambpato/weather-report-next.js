9d8f32b296a7d843ba8d92485abfb96d0cd82609
Lamberto Martinez <lambpato@gmail.com>
2023-12-17 09:26:46 +0000

    created function to fetch forecast infromation

--- weather-report/src/app/lib/index.ts
+++ weather-report/src/app/lib/index.ts
@@ -8,3 +8,14 @@ export const getSearch = async (query: string) => {
     console.error(e);
   }
 };
+
+export const getForecast = async (query: string) => {
+  try {
+    const response = await fetch(`https://api.weatherapi.com/v1/forecast.json?q=${query}&key=bb2468a183ea4225855173630232702`);
+    if (!response.ok) throw new Error(`Something bad happened ${response.status}`);
+    const jsonData = await response.json();
+    return jsonData;
+  } catch(e) {
+    console.error(e);
+  }
+}
