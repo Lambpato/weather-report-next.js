8d5bb54686a55ca1689a399b947a08f06dd59e0c
Lambpato <Lambpato@gmail.com>
2024-11-12 14:18:42 -0800

    updated styling for deskop in forecast directory

--- src/app/forecast/page.tsx
+++ src/app/forecast/page.tsx
@@ -46,13 +46,13 @@ export default function Page() {
             onClick={() => setIsMobilePreviewActive(!isMobilePreviewActive)}
           />
           <section>
-            <div className="grid place-items-center p-8">
-              <h1 className="text-3xl">{forecast.location.name}</h1>
-              <h3 className="text-2xl">{`${Math.round(
+            <div className="grid place-items-center p-8 gap-3">
+              <h1 className="text-3xl md:text-5xl">{forecast.location.name}</h1>
+              <h3 className="text-2xl md:text-4xl">{`${Math.round(
                 forecast.current.temp_f
               )}°`}</h3>
               <div className="w-1/2 h-full flex flex-row items-center justify-between">
-                <p className="text-lg font-medium">
+                <p className="text-lg font-medium md:text-xl">
                   {forecast.current.condition.text}
                 </p>
                 <Image
@@ -63,10 +63,10 @@ export default function Page() {
                 />
               </div>
               <div className="w-1/2 flex flex-row justify-between">
-                <p className="text-lg font-medium">{`H: ${Math.round(
+                <p className="text-lg font-medium md:text-xl">{`H: ${Math.round(
                   forecast.forecast.forecastday[0].day.maxtemp_f
                 )}°`}</p>
-                <p className="text-lg font-medium">{`L: ${Math.round(
+                <p className="text-lg font-medium md:text-xl">{`L: ${Math.round(
                   forecast.forecast.forecastday[0].day.mintemp_f
                 )}°`}</p>
               </div>
@@ -82,29 +82,37 @@ export default function Page() {
                 />
                 <div className="grid grid-cols-2 gap-5">
                   <div className="flex flex-col justify-between p-4 border border-cyan-600 rounded-2xl shadow-md shadow-cyan-600/45">
-                    <h3 className="text-2xl font-semibold">Condition</h3>
+                    <h3 className="text-2xl font-semibold md:text-3xl">
+                      Condition
+                    </h3>
                     <Image
                       src={`https:${forecast.current.condition.icon}`}
                       alt={`${forecast.current.condition.text} icon`}
                       width={75}
                       height={75}
                     />
-                    <p className="text-md font-medium">
+                    <p className="text-lg font-medium md:text-xl">
                       {forecast.current.condition.text}
                     </p>
                   </div>
                   <div className="flex flex-col p-4 border border-cyan-600 rounded-xl shadow-md shadow-cyan-600/45">
-                    <h3 className="text-2xl font-semibold">Wind Conditions</h3>
+                    <h3 className="text-2xl font-semibold md:text-3xl">
+                      Wind Conditions
+                    </h3>
                     <div className="h-full flex flex-col justify-around">
                       <div className="flex items-center">
-                        <p className="text-lg font-medium">Direction:&nbsp;</p>
-                        <p className="text-base font-medium">
+                        <p className="text-lg font-medium md:text-xl">
+                          Direction:&nbsp;
+                        </p>
+                        <p className="text-base font-medium md:text-xl">
                           {forecast.current.wind_dir}
                         </p>
                       </div>
                       <div className="flex items-center">
-                        <p className="text-lg font-medium">MPH:&nbsp;</p>
-                        <p className="text-base font-medium">
+                        <p className="text-lg font-medium md:text=xl">
+                          MPH:&nbsp;
+                        </p>
+                        <p className="text-base font-medium md:text-xl">
                           {forecast.current.wind_mph}
                         </p>
                       </div>
@@ -112,24 +120,30 @@ export default function Page() {
                   </div>
                 </div>
                 <div className="grid grid-cols-2 gap-5">
-                  <div className="w-auto p-4 border border-cyan-600 rounded-2xl shadow-md shadow-cyan-600/45">
-                    <h3 className="text-2xl font-semibold">Humidity</h3>
-                    <p className="text-lg font-medium">{`${forecast.current.humidity}%`}</p>
-                    <Image
-                      src="/humidity.svg"
-                      alt="Humidity"
-                      height={50}
-                      width={50}
-                      style={{
-                        filter:
-                          "invert(65%) sepia(15%) saturate(217%) hue-rotate(190deg) brightness(92%) contrast(85%) drop-shadow(0 0 0.1rem rgb(59, 59, 59))",
-                      }}
-                      className="drop-shadow-md"
-                    />
+                  <div className="w-auto flex flex-col justify-between p-4 border border-cyan-600 rounded-2xl shadow-md shadow-cyan-600/45">
+                    <h3 className="text-2xl font-semibold md:text-3xl">
+                      Humidity
+                    </h3>
+                    <div className="h-full flex flex-col justify-around">
+                      <Image
+                        src="/humidity.svg"
+                        alt="Humidity"
+                        height={35}
+                        width={35}
+                        style={{
+                          filter:
+                            "invert(65%) sepia(15%) saturate(217%) hue-rotate(190deg) brightness(92%) contrast(85%) drop-shadow(0 0 0.1rem rgb(59, 59, 59))",
+                        }}
+                        className="drop-shadow-md"
+                      />
+                      <p className="text-4xl md:text-6xl">{`${forecast.current.humidity}%`}</p>
+                    </div>
                   </div>
                   <div className="w-auto p-4 border border-cyan-600 rounded-2xl shadow-md shadow-cyan-600/45">
-                    <h3 className="text-2xl font-semibold">Chance of Rain</h3>
-                    <p className="text-lg font-medium">{`${forecast.forecast.forecastday[0].day.daily_chance_of_rain}%`}</p>
+                    <h3 className="text-2xl font-semibold md:text-3xl">
+                      Chance of Rain
+                    </h3>
+                    <p className="text-lg font-medium md:text-xl">{`${forecast.forecast.forecastday[0].day.daily_chance_of_rain}%`}</p>
                     <Image
                       src="https://cdn.weatherapi.com/weather/64x64/day/263.png"
                       alt="Chance of Rain"
@@ -140,31 +154,41 @@ export default function Page() {
                   </div>
                 </div>
                 <div className="w-full p-4 border border-cyan-600 rounded-2xl shadow-md shadow-cyan-600/45 justify-between">
-                  <h3 className="text-2xl text-center font-semibold">Astro</h3>
+                  <h3 className="text-2xl text-center font-semibold md:text-3xl">
+                    Astro
+                  </h3>
                   <div className="grid grid-cols-2 gap-3">
                     <div className="flex justify-between items-center">
-                      <p className="text-base font-semibold">Sunrise:</p>
-                      <p className="text-sm font-medium">
+                      <p className="text-base font-semibold md:text-lg">
+                        Sunrise:
+                      </p>
+                      <p className="text-sm font-medium md:text-base">
                         {forecast.forecast.forecastday[0].astro.sunrise}
                       </p>
                     </div>
                     <div className="flex justify-between items-center">
-                      <p className="text-base font-semibold">Sunset:</p>
-                      <p className="text-sm font-medium">
+                      <p className="text-base font-semibold md:text-lg">
+                        Sunset:
+                      </p>
+                      <p className="text-sm font-medium md:text-base">
                         {forecast.forecast.forecastday[0].astro.sunset}
                       </p>
                     </div>
                   </div>
                   <div className="grid grid-cols-2 gap-5">
                     <div className="flex justify-between items-center">
-                      <p className="text-base font-semibold">Moonrise:</p>
-                      <p className="text-sm font-medium">
+                      <p className="text-base font-semibold md:text-lg">
+                        Moonrise:
+                      </p>
+                      <p className="text-sm font-medium md:text-base">
                         {forecast.forecast.forecastday[0].astro.moonrise}
                       </p>
                     </div>
                     <div className="flex justify-between items-center">
-                      <p className="text-base font-semibold">Moonset:</p>
-                      <p className="text-sm font-medium">
+                      <p className="text-base font-semibold md:text-lg">
+                        Moonset:
+                      </p>
+                      <p className="text-sm font-medium md:text-base">
                         {forecast.forecast.forecastday[0].astro.moonset}
                       </p>
                     </div>
